04db013a2dd94796baeef9e8cb9d4c223a545231
Kelwin Payares <kpayares@vmware.com>
2022-03-24 16:08:40 +0100

    add mongo db integration

--- src/main/java/com/vmware/acmecatalog/model/Product.java
+++ src/main/java/com/vmware/acmecatalog/model/Product.java
@@ -1,24 +1,25 @@
 package com.vmware.acmecatalog.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Document(collection = "product")
-public class Product {
+public class Product extends ProductBase {
 
     @Id
+    @JsonIgnore
     private String id;
-    private String name;
-    private String shortDescription;
-    private String description;
+
+    @JsonProperty("imageurl1")
     private String imageUrl1;
+
+    @JsonProperty("imageurl2")
     private String imageUrl2;
+
+    @JsonProperty("imageurl3")
     private String imageUrl3;
-    private String price;
-    private List<String> tags = new ArrayList<>();
 
     public String getId() {
         return id;
@@ -26,30 +27,6 @@ public class Product {
 
     public void setId(String id) {
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getShortDescription() {
-        return shortDescription;
-    }
-
-    public void setShortDescription(String shortDescription) {
-        this.shortDescription = shortDescription;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public String getImageUrl1() {
@@ -74,21 +51,5 @@ public class Product {
 
     public void setImageUrl3(String imageUrl3) {
         this.imageUrl3 = imageUrl3;
-    }
-
-    public String getPrice() {
-        return price;
-    }
-
-    public void setPrice(String price) {
-        this.price = price;
-    }
-
-    public List<String> getTags() {
-        return tags;
-    }
-
-    public void setTags(List<String> tags) {
-        this.tags = tags;
     }
 }
---
+++ src/main/java/com/vmware/acmecatalog/request/ProductRequest.java
@@ -0,0 +1,10 @@
+package com.vmware.acmecatalog.request;
+
+import com.vmware.acmecatalog.model.ProductBase;
+
+public class ProductRequest extends ProductBase {
+
+    private String imageurl1;
+    private String imageurl2;
+    private String imageurl3;
+}
